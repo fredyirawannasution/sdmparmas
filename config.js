ed6d57ab562925b7db2a97ff3490eb8d105964c6
fredyirawannasution <fredyirawannasution@gmail.com>
2026-04-28 10:00:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Contoh format: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
 window.TEKHUM_CONFIG = {
   APP_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyjCME2lGLU5AgOztEm9zyQydk_pQhSXi7qFuG_xLEEg76Vg5lvCVPyLQDskx6xeDqU/exec',
-  APP_TITLE: 'Pengelolaan Dokumen Teknis & Hukum',
+  APP_TITLE: 'Pengelolaan SDM dan Parmas',
   ORG_NAME: 'KPU Kabupaten Labuhanbatu Utara'
 };
