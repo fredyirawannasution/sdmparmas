74313e373890b2798ca64e7a28e388ee5a0431ad
fredyirawannasution <fredyirawannasution@gmail.com>
2026-05-06 14:45:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Contoh format: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
 window.SDMPARMAS_CONFIG = {
   APP_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbw3Kte8Q7_OeHZW7ELl2YS1JcsmTJZsBBONf_aF3V91vFCZFiGZ8vX0QQdb3y6zaMNE/exec',
-  APP_TITLE: 'Pengelolaan SDM dan Parmas',
+  APP_TITLE: 'Pengelolaan Parmas dan SDM',
   ORG_NAME: 'KPU Kabupaten Labuhanbatu Utara'
 };
