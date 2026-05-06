0e38aa5bcb9449b5f3a23ee9566fec8d182e7b97
fredyirawannasution <fredyirawannasution@gmail.com>
2026-05-06 14:36:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Ganti URL ini dengan URL deployment Google Apps Script Web App Anda.
 // Contoh format: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
 window.SDMPARMAS_CONFIG = {
-  APP_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbx4z8sjUbMrUbuO2umR4VNJja6SISnNrz94t3u8H6qGdFxUYPELVBWqWQSpK4TkgVKF/exec',
+  APP_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbw3Kte8Q7_OeHZW7ELl2YS1JcsmTJZsBBONf_aF3V91vFCZFiGZ8vX0QQdb3y6zaMNE/exec',
   APP_TITLE: 'Pengelolaan SDM dan Parmas',
   ORG_NAME: 'KPU Kabupaten Labuhanbatu Utara'
 };
